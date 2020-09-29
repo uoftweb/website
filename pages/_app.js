13c9fe7ed979a248117cf21204e8d3d6c5ef0841
Umar Ahmed <umar.ahmed@mail.utoronto.ca>
2020-09-29 14:22:45 -0400

    feat: add icons and site metadata

--- pages/_app.js
+++ pages/_app.js
@@ -4,12 +4,34 @@ import Head from "next/head";
 
 function App({ Component, pageProps }) {
   return (
+    <>
+      <Head>
+        <meta content="IE=edge" httpEquiv="X-UA-Compatible" />
+        <meta content="width=device-width, initial-scale=1" name="viewport" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <title>UofT Web Development Club</title>
+      </Head>
     <AuthProvider session={pageProps.session}>
       <ThemeProvider>
         <CSSReset />
-        <Head>
-          <title>UofT Web Development Club</title>
-        </Head>
         <Component {...pageProps} />
       </ThemeProvider>
     </AuthProvider>
