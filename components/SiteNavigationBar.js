30de377fff50fe1b09f232d0471e68cf6fbac0cd
Umar Ahmed <umar.ahmed@mail.utoronto.ca>
2020-10-01 16:36:33 -0400

    feat: make navbar links route aware

--- components/SiteNavigationBar.js
+++ components/SiteNavigationBar.js
@@ -16,6 +16,7 @@ import { siteConfig } from "configs/site";
 import { useColorModeValue } from "hooks/chakra";
 import { signIn, signOut, useSession } from "next-auth/client";
 import NextLink from "next/link";
+import { useRouter } from "next/router";
 
 const DiscordIcon = (props) => (
   <svg viewBox="0 0 146 146" style={{ height: "1em", width: "1em" }} {...props}>
@@ -37,9 +38,10 @@ const GithubIcon = (props) => (
 );
 
 function SiteNavigationBarLink({ href, children, isExternal }) {
+  const router = useRouter();
   return (
     <Link as={NextLink} href={href} isExternal>
-      <Button variantColor="brand">
+      <Button variantColor="brand" isActive={href === router.asPath}>
         {children} {isExternal && <Icon name="external-link" mx="2px" />}
       </Button>
     </Link>
@@ -62,61 +64,69 @@ export function SiteNavigationBar() {
         py={4}
         px={8}
       >
-        {/* Logo */}
-        <Link as={NextLink} href={session?.user ? "/dashboard" : "/"}>
-          <a>
-            <Box height={12} width={12}>
-              <svg
-                viewBox="0 0 64 64"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-                aria-labelledby="logoTitle"
-              >
-                <title id="logoTitle">UofT Web Dev Club</title>
-                <rect width="64" height="64" rx="8" fill="#fff" opacity={0.2} />
-                <g clipPath="url(#clip0)">
-                  <path
-                    fillRule="evenodd"
-                    clipRule="evenodd"
-                    d="M51.636 46.419c-3.704-1.844-11.198-4.95-19.636-4.95-8.43 0-15.924 3.103-19.636 4.95V23.824h-2.182v23.766c-.188.11-.285.17-.285.17C8.849 48.345 8 47.836 8 46.627V17.193a2.21 2.21 0 01.643-1.553c.41-.411.966-.641 1.544-.64h43.626a2.174 2.174 0 011.543.64c.41.411.642.97.644 1.553v29.434c0 1.212-.844 1.705-1.911 1.124 0 0-.093-.057-.27-.162V23.822h-2.183v22.597zm-41.454-28.48c0-.405.326-.733.72-.733h42.196c.398 0 .72.328.72.733v5.884H10.183v-5.884zm9.09 3.678c.603 0 1.092-.494 1.092-1.103s-.49-1.103-1.091-1.103c-.602 0-1.091.495-1.091 1.104 0 .608.489 1.103 1.09 1.103v-.001zm-2.908 0c.602 0 1.09-.494 1.09-1.103s-.488-1.103-1.09-1.103c-.603 0-1.091.495-1.091 1.104 0 .608.489 1.103 1.09 1.103v-.001zm-2.91 0c.603 0 1.091-.494 1.091-1.103s-.488-1.103-1.09-1.103c-.603 0-1.091.495-1.091 1.104 0 .608.488 1.103 1.09 1.103v-.001z"
+        <Stack isInline align="center" spacing={8} shouldWrapChildren>
+          {/* Logo */}
+          <Link as={NextLink} href={session?.user ? "/dashboard" : "/"}>
+            <a>
+              <Box height={12} width={12}>
+                <svg
+                  viewBox="0 0 64 64"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                  aria-labelledby="logoTitle"
+                >
+                  <title id="logoTitle">UofT Web Dev Club</title>
+                  <rect
+                    width="64"
+                    height="64"
+                    rx="8"
                     fill="#fff"
+                    opacity={0.2}
                   />
-                </g>
-                <defs>
-                  <clipPath id="clip0">
+                  <g clipPath="url(#clip0)">
                     <path
+                      fillRule="evenodd"
+                      clipRule="evenodd"
+                      d="M51.636 46.419c-3.704-1.844-11.198-4.95-19.636-4.95-8.43 0-15.924 3.103-19.636 4.95V23.824h-2.182v23.766c-.188.11-.285.17-.285.17C8.849 48.345 8 47.836 8 46.627V17.193a2.21 2.21 0 01.643-1.553c.41-.411.966-.641 1.544-.64h43.626a2.174 2.174 0 011.543.64c.41.411.642.97.644 1.553v29.434c0 1.212-.844 1.705-1.911 1.124 0 0-.093-.057-.27-.162V23.822h-2.183v22.597zm-41.454-28.48c0-.405.326-.733.72-.733h42.196c.398 0 .72.328.72.733v5.884H10.183v-5.884zm9.09 3.678c.603 0 1.092-.494 1.092-1.103s-.49-1.103-1.091-1.103c-.602 0-1.091.495-1.091 1.104 0 .608.489 1.103 1.09 1.103v-.001zm-2.908 0c.602 0 1.09-.494 1.09-1.103s-.488-1.103-1.09-1.103c-.603 0-1.091.495-1.091 1.104 0 .608.489 1.103 1.09 1.103v-.001zm-2.91 0c.603 0 1.091-.494 1.091-1.103s-.488-1.103-1.09-1.103c-.603 0-1.091.495-1.091 1.104 0 .608.488 1.103 1.09 1.103v-.001z"
                       fill="#fff"
-                      transform="translate(8 15)"
-                      d="M0 0h48v33H0z"
                     />
-                  </clipPath>
-                </defs>
-              </svg>
-            </Box>
-          </a>
-        </Link>
+                  </g>
+                  <defs>
+                    <clipPath id="clip0">
+                      <path
+                        fill="#fff"
+                        transform="translate(8 15)"
+                        d="M0 0h48v33H0z"
+                      />
+                    </clipPath>
+                  </defs>
+                </svg>
+              </Box>
+            </a>
+          </Link>
 
-        {/* Navigation Menu */}
-        <Stack isInline shouldWrapChildren spacing={6}>
-          <SiteNavigationBarLink href="/articles">
-            Articles
-          </SiteNavigationBarLink>
-          {features.workshops && (
-            <SiteNavigationBarLink href="/workshops">
-              Workshops
+          {/* Navigation Menu */}
+          <Stack isInline shouldWrapChildren spacing={4}>
+            <SiteNavigationBarLink href="/articles">
+              Articles
             </SiteNavigationBarLink>
-          )}
-          {features.projects && (
-            <SiteNavigationBarLink href="/projects">
-              Projects
+            {features.workshops && (
+              <SiteNavigationBarLink href="/workshops">
+                Workshops
+              </SiteNavigationBarLink>
+            )}
+            {features.projects && (
+              <SiteNavigationBarLink href="/projects">
+                Projects
+              </SiteNavigationBarLink>
+            )}
+            <SiteNavigationBarLink
+              href="https://trello.com/b/p5fiez3v/public-roadmap"
+              isExternal
+            >
+              Roadmap
             </SiteNavigationBarLink>
-          )}
-          <SiteNavigationBarLink
-            href="https://trello.com/b/p5fiez3v/public-roadmap"
-            isExternal
-          >
-            Roadmap
-          </SiteNavigationBarLink>
+          </Stack>
         </Stack>
 
         {/* Actions */}
