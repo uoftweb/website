c4fda2953dc2def7dec8a748110e5c480fae6010
Umar Ahmed <8302959+umar-ahmed@users.noreply.github.com>
2020-11-30 02:06:56 +0000

    refactor: use extendTheme

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 import "focus-visible/dist/focus-visible";
 
-import { ChakraProvider, theme } from "@chakra-ui/react";
+import { ChakraProvider } from "@chakra-ui/react";
 import { Provider as AuthProvider } from "next-auth/client";
 import Head from "next/head";
 import Router from "next/router";
@@ -9,55 +9,13 @@ import "nprogress/nprogress.css";
 import { ApolloProvider } from "@apollo/client";
 
 import { useApollo } from "../lib/apolloClient";
+import { theme } from "../theme";
 
 NProgress.configure({ showSpinner: false, parent: "#site-header" });
 
 Router.events.on("routeChangeStart", () => NProgress.start());
 Router.events.on("routeChangeComplete", () => NProgress.done());
 Router.events.on("routeChangeError", () => NProgress.done());
-
-const customTheme = {
-  ...theme,
-  colors: {
-    ...theme.colors,
-    brand: {
-      50: "#dfeeff",
-      100: "#b0cbff",
-      200: "#7ea8ff",
-      300: "#4b86ff",
-      400: "#1a63ff",
-      500: "#004ae6",
-      600: "#0039b4",
-      700: "#002982",
-      800: "#001951",
-      900: "#000821",
-    },
-    accent: {
-      50: "#e1ffec",
-      100: "#b8f6d1",
-      200: "#8fefb4",
-      300: "#64e898",
-      400: "#3ae17c",
-      500: "#23c862",
-      600: "#179b4c",
-      700: "#0c6f35",
-      800: "#03431e",
-      900: "#001806",
-    },
-  },
-  fonts: {
-    ...theme.fonts,
-    body: "Inter, system-ui, sans-serif",
-    heading: "Inter, system-ui, sans-serif",
-    mono: "'Fira Code', Menlo, monospace",
-  },
-  radii: {
-    ...theme.radii,
-    xl: "1.0rem",
-    "2xl": "2.0rem",
-    "3xl": "3.0rem",
-  },
-};
 
 function App({ Component, pageProps }) {
   const apolloClient = useApollo(pageProps.initialApolloState);
@@ -89,7 +47,7 @@ function App({ Component, pageProps }) {
       </Head>
       <ApolloProvider client={apolloClient}>
         <AuthProvider session={pageProps.session}>
-          <ChakraProvider theme={customTheme}>
+          <ChakraProvider theme={theme}>
             <Component {...pageProps} />
           </ChakraProvider>
         </AuthProvider>
