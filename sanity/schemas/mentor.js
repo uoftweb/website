a38a693d848a989e65b55b65440de501227b2f0d
MaXeraph <maxpham99@gmail.com>
2021-01-23 17:38:09 -0500

    Add mentor schema

---
+++ sanity/schemas/mentor.js
@@ -0,0 +1,54 @@
+export default {
+  name: "mentor",
+  title: "Mentor",
+  type: "document",
+  fields: [
+    {
+      name: "name",
+      title: "Name",
+      type: "string",
+    },
+    {
+      name: "slug",
+      title: "Slug",
+      type: "slug",
+      options: {
+        source: "name",
+        maxLength: 96,
+      },
+    },
+    {
+      name: "skills",
+      title: "Skills",
+      type: "array",
+      of: [{ type: "string" }],
+    },
+    {
+      name: "image",
+      title: "Image",
+      type: "image",
+      options: {
+        hotspot: true,
+      },
+    },
+    {
+      name: "bio",
+      title: "Bio",
+      type: "array",
+      of: [
+        {
+          title: "Block",
+          type: "block",
+          styles: [{ title: "Normal", value: "normal" }],
+          lists: [],
+        },
+      ],
+    },
+  ],
+  preview: {
+    select: {
+      title: "name",
+      media: "image",
+    },
+  },
+};
