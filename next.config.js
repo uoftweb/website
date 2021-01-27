dcc091ac1c3018906587503b83d79e90820d40fc
Umar Ahmed <umar.ahmed@mail.utoronto.ca>
2021-01-27 12:50:00 -0500

    feat: add design resources redirect

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 module.exports = {
+  async redirects() {
+    return [
+      {
+        source: "/resources/design",
+        destination:
+          "https://docs.google.com/document/d/1Md0Ba5wPPw-Qeu1-FOtnF_qV7ltgYCyhkKA63P3K9kE",
+        permanent: false,
+      },
+    ];
+  },
   webpack: (config, { isServer }) => {
     // Fixes npm packages that depend on `fs` module
     if (!isServer) {
