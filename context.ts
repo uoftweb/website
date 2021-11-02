260acbeddc1ad060996ac6e8f54db0b8a08f4a09
JamesYL <james.yung.lee@gmail.com>
2021-11-02 14:06:56 -0400

    prisma removed

--- context.ts
+++
@@ -1,9 +0,0 @@
-import { PrismaClient } from "@prisma/client";
-import { Session } from "next-auth/client";
-
-export type WDCSession = Session & { user: { id: number } };
-
-export type Context = {
-  prisma: PrismaClient;
-  session: WDCSession;
-};
